21a687f476556597dd432a81879a4d20de9ef7be
seyiodediran <odediran_s@yahoo.com>
2020-08-16 21:55:56 +0100

    heroku commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
-
-
 const express = require('express')
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const multer = require("multer");
 // const Article = require('./models/article')
 // const articleRouter = require('./routes/articles')
 // const methodOverride = require('method-override')
@@ -22,15 +20,31 @@ app.use(session({
 	secret: "any random string"
 }))
 
+var MongoClient = require("mongodb").MongoClient;
 
+// const connectionstring = "mongodb://localhost:27017"
+const connectionstring = "mongodb://herokulogin:herokulogin123@ds139896.mlab.com:39896/heroku_719llv9f"
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hephzibah', {useNewUrlParser: true, useUnifiedTopology: true})
+MongoClient.connect(connectionstring, {useNewUrlParser: true, useUnifiedTopology: true}, function (error,client){
 
-
-
-	var blog = client.db("blog");
+	if(error) { console.log(error); return; }
+	//var blog = client.db("blog");
+	var blog = client.db("heroku_719llv9f");
+	
 	console.log("DB connected");
 
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'views/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
 
 
 
@@ -215,34 +229,72 @@ app.post("/do-comment", function (req, res) {
 
 
 
-app.post("/do-upload-image", function (req, res) {
-	var formData = new formidable.IncomingForm();
-	formData.parse(req, function (error, fields, files) {
-		var oldPath = files.file.path;
-		var newPath = "views/images/" + files.file.name;
+app.post("/do-upload-image", upload.single('file'), function (req, res) {
 
-		fs.rename (oldPath, newPath, function(err) {
-			res.send("/" + newPath);
-		})
+
+	const file = req.file;
+
+	if(file === null || file === undefined) {
+
+		res.status(400).send("No file uploaded")
+
+	}
+
+	res.send(file.path);
+	
+
+
+
+	// var formData = new formidable.IncomingForm();
+	// formData.parse(req, function (error, fields, files) {
+
+	// 	console.log("fields", fields);
+
+	// 	console.log("files", files);
+
+	// 	var oldPath = files.file.path;
+	// 	var newPath = "views/images/" + files.file.name;
+
+	// 	fs.rename (oldPath, newPath, function(err) {
+	// 		res.send("/" + newPath);
+	// 	})
 		
-	})
+	// })
 }) 
 
-app.post("/do-update-image", function(req, res) {
-	var formData = new formidable.IncomingForm();
-	formData.parse(req, function (error, fields, files) {
+app.post("/do-update-image", upload.single('file'),function(req, res) {
 
-		fs.unlink(fields.image.replace("/", ""),function(error) {
-			var oldPath = files.file.path;
-		var newPath = "views/images/" + files.file.name;
+	const file = req.file;
 
-		fs.rename (oldPath, newPath, function(err) {
-			res.send("/" + newPath);
-		})
-		})
+	if(file === null || file === undefined) {
+
+		res.status(400).send("No file uploaded")
+
+	}
+
+	console.log(req.body);
+
+	// fs.unlink(fields.image.replace("/", ""),function(error) {
+	// var oldPath = file.path
+
+	res.send(file.path);
+
+
+
+
+	// var formData = new formidable.IncomingForm();
+	// formData.parse(req, function (error, fields, files) {
+
+	// 	fs.unlink(fields.image.replace("/", ""),function(error) {
+	// 		var oldPath = files.file.path;
+	// 	var newPath = "views/images/" + files.file.name;
+
+	// 	fs.rename (oldPath, newPath, function(err) {
+	// 		res.send("/" + newPath);
+	// 	})
+	// })
 		
-		
-	})
+	// })
 })
 
 
@@ -265,14 +317,11 @@ io.on("connection", function (socket) {
 })
 
 
-
-const port = process.env.PORT || 3000;
-
-
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 app.listen(port);
 
 
-
-
-
+});
